fc971454a89f31a3aca3a329fef9e293343523eb
Remon Abdou <remoo1901@hotmail.com>
2020-07-08 22:09:59 -0400

    working on users-routers

---
+++ database/middleware/restrict.js
@@ -0,0 +1,21 @@
+const Users = require("../../users/users-model");
+
+function restrict() {
+  const authError = {
+    message: "INVALID CREDENTIALS",
+  };
+
+  return async (req, res, next) => {
+    try {
+      if (!req.session || !req.session.user) {
+        return res.status(401).json(authError);
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+
+module.exports = restrict;
--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,19 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: "./database/auth.db3"
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done)
+      },
+    },
+    
   },
 
   staging: {
