d7716849b6970ddde7edf94f8ee89c4c00c69892
Remon Abdou <remoo1901@hotmail.com>
2020-07-08 20:28:19 -0400

    finished index.js and knexfile.js

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const session = require("express-session")
+const KnexSessionStore = require("connect-session-knex")(session)
+const db = require("./database/config")
+const userRouter = require("./users/users-router")
+
+const server = express()
+const port = process.env.PORT || 5000
+
+server.use(helmet())
+server.use(cor())
+server.use(express.json())
+server.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "Kepp it secret, keep it safe",
+    store: new KnexSessionStore({
+        knex: db,
+        createtable: true,
+    })
+}))
+
+server.use(userRouter)
+
+server.use((err, req, res, next) => {
+    console.log(err)
+
+    res.status(500).json({
+        Message: "Something went wrong",
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`)
+})
